0ec178632dab93877dc72dd1f1859daa154af9ef
yowgf <alexthomasmol@gmail.com>
2022-05-01 15:23:51 -0300

    Crawling finally starting to look good

--- crawler/_internal/crawler.py
+++ crawler/_internal/crawler.py
@@ -1,9 +1,15 @@
 import json
+import multiprocessing as mp # TODO
 import time
 
+# Crawling libraries
 from bs4 import BeautifulSoup
 from bs4.element import Comment as bs4_comment
-import requests
+# import requests
+import urllib3
+from url_normalize import url_normalize
+from reppy.robots import Robots
+from warcio.capture_http import capture_http
 
 from . import log
 
@@ -24,6 +30,9 @@ class Crawler:
         except FileNotFoundError as e:
             logger.error(f"error reading seeds file '{seeds_file}': {e}")
             raise
+
+        self.http_pool = urllib3.PoolManager()
+        self.crawled_pages = []
 
     def is_relevant_text(self, soup_element):
         if soup_element.parent.name in Crawler._nontext_tags:
@@ -49,7 +58,7 @@ class Crawler:
     # the first relevant text, instead of a list
     def find_relevant_text(self, soup):
         texts = soup.findAll(text=True)
-
+        
         relevant_text = ""
         for element in texts:
             if self.is_relevant_text(element):
@@ -65,30 +74,74 @@ class Crawler:
         first_20words_relevant_text = str(relevant_text).split(" ")[:20]
         return " ".join(first_20words_relevant_text)
 
-    def print_debug(self, resp):
+    def print_debug(self, url, soup):
         debug_json_obj = {}
-
-        soup = BeautifulSoup(resp.text, 'html.parser')
-
-        debug_json_obj['URL'] = resp.url
+        debug_json_obj['URL'] = url
         debug_json_obj['Title'] = str(soup.title.string)
         debug_json_obj['Text'] = self.find_relevant_text(soup)
         debug_json_obj['Timestamp'] = int(time.time())
 
         print(json.dumps(debug_json_obj))
 
-    def crawl(self, seed, max_depth=None):
-        logger.debug('Making request to url ' + seed)
+    def normalize(self, parent_url, url):
+        logger.debug(f"Normalizing url {url} with parent {parent_url}")
 
-        resp = requests.get(seed)
+        normalized_url = None
 
-        logger.debug('Got respose: ' + str(resp))
-#        logger.debug('Got html content: ' + resp.text)
+        if url.startswith("/"):
+            normalized_url = parent_url.strip("/") + url
+        elif url.startswith("./"):
+            normalized_url = parent_url.strip("/") + url[1:]
+        else:
+            normalized_url = url
 
+        normalized_url = url_normalize(normalized_url)
+
+        if normalized_url in self.crawled_pages:
+            return None, True, "page has already been crawled"
+
+        logger.debug(f"Normalized url: {normalized_url}")
+
+        return normalized_url, False, ""
+
+    def crawl(self, parent_url, url, max_depth=10):
+        # Fire wall
+        if max_depth == 0:
+            return
+        if url == None:
+            url = parent_url
+
+        url, should_skip, reason = self.normalize(parent_url, url)
+        if should_skip:
+            logger.debug(f"Skipping url {url}. Reason: {reason}")
+            return
+
+        logger.debug(f"Crawling url: {url}")
+
+        # Wait a bit to avoid being blocked
+        time.sleep(0.2)
+            
+        resp = self.http_pool.request('GET', url)
+        self.crawled_pages.append(url)
+        logger.debug('Got response: ' + str(resp))
+        
+        soup = BeautifulSoup(resp.data, 'html.parser')
+        
         if self._config.debug:
-            self.print_debug(resp)
+            self.print_debug(url, soup)
+            
+        links = soup.findAll('a')
+        child_urls = [link.attrs.get('href') for link in links
+                      if link.attrs.get('href') != None and
+                      not url.startswith("/") and
+                      not url.startswith("./")
+        ]
+        
+        for child_url in child_urls:
+            self.crawl(url, child_url, max_depth=max_depth-1)
 
     # run assumes that Crawler.init has already been called upon the object.
     def run(self):
-        for seed in self.seeds:
-            self.crawl(seed, max_depth=10)
+        with capture_http('crawled_pages.gz'):
+            for seed in self.seeds:
+                self.crawl(seed, None, max_depth=10)
